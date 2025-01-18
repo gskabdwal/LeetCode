e6bebdfd7eadffc0f0a2be2d982bb7419fc01a93
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-18 11:58:30 +0530

    Time: 0 ms (100%), Space: 15.2 MB (73.68%) - LeetHub

--- 0372-super-pow/0372-super-pow.cpp
+++ 0372-super-pow/0372-super-pow.cpp
@@ -1,24 +1,20 @@
 class Solution {
-    const int base = 1337;
-    
-    int powmod(int a, int k) //a^k mod 1337 where 0 <= k <= 10
-    {
-        if(k==0) return 1;
-        a %= base;
-        int result = powmod(a,k/2)%base;
-        result = (result*result)%base;
-        if(k%2) result = (result*a)%base;
-        return result;
+  
+    int power(int x, int n, int mod) {
+        int ret = 1;
+        for (x %= mod; n; x = x * x % mod, n >>= 1) 
+            if (n & 1) ret = ret * x % mod;
+        return ret;
     }
 
-public:
-    
+  public:
+
     int superPow(int a, vector<int>& b) {
-        
-        if (b.empty()) return 1;
-        int last_digit = b.back();
-        b.pop_back();
-        return powmod(superPow(a, b), 10) * powmod(a, last_digit) % base;
-    
+        if (a % 1337 == 0) return 0; // this line could also be removed
+        int p = 0;
+        for (int i : b) p = (p * 10 + i) % 1140;
+        if (p == 0) p += 1140;
+        return power(a, p, 1337);
     }
+
 };
