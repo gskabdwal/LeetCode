ddb5b909f4d0625ce7d152604533810faf5e378a
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-20 10:00:57 +0530

    Time: 37 ms (70.78%), Space: 62.4 MB (78.21%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,5 +1,21 @@
 class Solution {
 
+    vector<vector<int>> iterativeCombine(int n, int k) {
+		vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
+
     void combine(vector<vector<int> > &res,vector<int> &temp,int start,int num,int n ,int k){
         if(num>=k){
             res.push_back(temp);
