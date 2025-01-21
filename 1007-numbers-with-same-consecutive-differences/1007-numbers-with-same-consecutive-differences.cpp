06087f00bb558ef0767d8475f433f513731209e6
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-21 09:49:44 +0530

    Time: 0 ms (100%), Space: 10.7 MB (56.62%) - LeetHub

--- 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/1007-numbers-with-same-consecutive-differences.cpp
@@ -7,17 +7,18 @@ class Solution {
             return;
         }
 
-        for(int j = (i==0?1:0) ; j<10; j++){
-            if(num==0 || abs(num%10-j)==k ){
-                getNums(n,k,ans,i+1, num*10+j);
-            }
-        }
+        int dig = num%10;
+        
+        if(dig+k<=9) getNums(n,k,ans,i+1,num*10+dig+k);
+        if(k!=0 && dig>=k) getNums(n,k,ans,i+1,num*10+dig-k);
+
     }
 
 public:
     vector<int> numsSameConsecDiff(int n, int k) {
         vector<int> ans;
-        getNums(n,k,ans,0, 0);
+        for(int num = 1 ; num<10; num++)
+            getNums(n,k,ans,1, num);
         return ans;
     }
 };
