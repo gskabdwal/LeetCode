2dde2d132e9ea185e6ed8be44d347862e28c3dd6
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-22 08:42:02 +0530

    Time: 0 ms (100%), Space: 11 MB (35.84%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -23,6 +23,28 @@ public:
         inorder(root->right,ans);
     }
 
+    void morrisInorder(TreeNode* root, vector<int>& nodes){
+        
+        while (root) {
+            if (root -> left) {
+                TreeNode* pre = root -> left;
+                while (pre -> right && pre -> right != root) {
+                    pre = pre -> right;
+                }
+                if (!pre -> right) {
+                    pre -> right = root;
+                    root = root -> left;
+                } else {
+                    pre -> right = NULL;
+                    nodes.push_back(root -> val);
+                    root = root -> right;
+                }
+            } else {
+                nodes.push_back(root -> val);
+                root = root -> right;
+            }
+        }
+    }
 
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> nodes;
