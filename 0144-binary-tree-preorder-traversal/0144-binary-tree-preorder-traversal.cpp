cf50c39e1cd123b2782b71272fbd9f21f88ae917
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-22 08:48:49 +0530

    Time: 0 ms (100%), Space: 11 MB (42.37%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -21,7 +21,28 @@ class Solution {
 
     }
 
-
+    void morrisPreorder(TreeNode* root, vector<int> &nodes){
+        while (root) {
+            if (root -> left) {
+                TreeNode* pre = root -> left;
+                while (pre -> right && pre -> right != root) {
+                    pre = pre -> right;
+                }
+                if (!pre -> right) {
+                    nodes.push_back(root->val);
+                    pre -> right = root;
+                    root = root -> left;
+                } else {
+                    pre -> right = NULL;
+                    // nodes.push_back(root -> val);
+                    root = root -> right;
+                }
+            } else {
+                nodes.push_back(root -> val);
+                root = root -> right;
+            }
+        }
+    }
 
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -29,19 +50,21 @@ public:
         if(!root) return {};
 
         vector<int> ans;
-        stack<TreeNode*> st;
+        // stack<TreeNode*> st;
 
-        auto node = root;
+        // auto node = root;
 
-        while(node){
+        // while(node){
 
-            ans.push_back(node->val);
+        //     ans.push_back(node->val);
             
-            if(node->right) st.push(node->right);
-            node = node->left;
-            if(!node && st.size()) node = st.top(), st.pop();
+        //     if(node->right) st.push(node->right);
+        //     node = node->left;
+        //     if(!node && st.size()) node = st.top(), st.pop();
 
-        }
+        // }
+
+        morrisPreorder(root,ans);
 
         return ans;
     }
