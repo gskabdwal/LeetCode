507434a799ebbe8c97df43cada4f00b1cda3ff7c
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-22 07:43:07 +0530

    Time: 15 ms (94%), Space: 40 MB (36.44%) - LeetHub

--- 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
@@ -1,22 +1,34 @@
 class Solution {
-
-    int solve(vector<int>& nums,int k, int i, unordered_map<int,int>& st){
-
-        if(i>=nums.size()){
+private:
+    int beautifulSubsets(vector<pair<int, int>>& s, int n, int k, int i, vector<int>& count) {
+        if (i == n) {
             return 1;
         }
-
-        int res = solve(nums,k,i+1,st);
-        if(!st[nums[i]-k]&&!st[nums[i]+k]){
-            st[nums[i]]++;
-            res += solve(nums,k,i+1,st);
-            st[nums[i]]--;
+        if (count[i] != -1) {
+            return count[i];
         }
-        return res;
+        int skip = beautifulSubsets(s, n, k, i + 1, count);
+        int take = (1 << s[i].second) - 1;
+        if (i + 1 < n && s[i + 1].first - s[i].first == k) {
+            take *= beautifulSubsets(s, n, k, i + 2, count);
+        } else {
+            take *= beautifulSubsets(s, n, k, i + 1, count);
+        }
+        return count[i] = skip + take;
     }
+
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
-        unordered_map<int,int> hs;
-        return solve(nums,k,0,hs)-1;      
+        int result = 1;
+        map<int, map<int, int>> freq;
+        for (int& x: nums) {
+            freq[x % k][x]++;
+        }
+        for (auto& fr: freq) {
+            vector<pair<int, int>> s(fr.second.begin(), fr.second.end());
+            vector<int> count(s.size(), -1);
+            result *= beautifulSubsets(s, s.size(), k, 0, count);
+        }
+        return result - 1;
     }
 };
