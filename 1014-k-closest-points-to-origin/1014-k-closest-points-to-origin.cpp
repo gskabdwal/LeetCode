2d2544f57b1d4f007434372ddbd3b8cfa3a5a17d
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-28 09:25:21 +0530

    Time: 64 ms (50.78%), Space: 74.8 MB (50.16%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,4 +1,13 @@
 class Solution {
+
+private:
+
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] > q[0] * q[0] + q[1] * q[1];
+        }
+    };
+
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
         priority_queue<vector<int>, vector<vector<int>>, compare> pq(points.begin(), points.end());
@@ -9,10 +18,4 @@ public:
         }
         return ans;
     }
-private:
-    struct compare {
-        bool operator()(vector<int>& p, vector<int>& q) {
-            return p[0] * p[0] + p[1] * p[1] > q[0] * q[0] + q[1] * q[1];
-        }
-    };
 };
