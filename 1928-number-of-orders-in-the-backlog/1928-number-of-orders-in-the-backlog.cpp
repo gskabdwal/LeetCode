ed7a4f5dc62ac122c55df202dea108dfefb7a1f1
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-29 09:21:47 +0530

    Time: 230 ms (21.33%), Space: 74 MB (22.22%) - LeetHub

--- 1928-number-of-orders-in-the-backlog/1928-number-of-orders-in-the-backlog.cpp
+++ 1928-number-of-orders-in-the-backlog/1928-number-of-orders-in-the-backlog.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-        int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
-        priority_queue<vector<int>>buy;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>sell;
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        typedef long long ll;
+        long long sum = 0, mod = 1e9 + 7;
+        priority_queue<vector<ll>, vector<vector<ll>>, greater<vector<ll>>> sq; // Min-heap for sell orders
+        priority_queue<vector<ll>> bq; // Max-heap for buy orders
+
         for (auto& o : orders) {
-            if (o[2] == 0)
-                buy.push(o);
-            else
-                sell.push(o);
-            while (buy.size() && sell.size() && sell.top()[0] <= buy.top()[0]) {
-                int k = min(buy.top()[1], sell.top()[1]);
-                vector<int> tmp = buy.top(); buy.pop();
-                tmp[1] -= k;
-                if (tmp[1]) buy.push(tmp);
+            ll p = o[0], a = o[1], t = o[2];
 
-                tmp = sell.top(); sell.pop();
-                tmp[1] -= k;
-                if (tmp[1]) sell.push(tmp);
+            if (t == 0) {  // Buy order
+                while (!sq.empty() && p >= sq.top()[0] && a > 0) {
+                    int sub = min(a, sq.top()[1]);
+                    auto s = sq.top();
+                    sq.pop();
+                    s[1] -= sub;
+                    a -= sub;
+                    sum = (sum - sub + mod) % mod;  // Track remaining backlog orders
+                    if (s[1] > 0)
+                        sq.push(s);
+                }
+                if (a > 0) {
+                    bq.push({p, a});
+                    sum = (sum + a) % mod;
+                }
+            } 
+            else {  // Sell order
+                while (!bq.empty() && p <= bq.top()[0] && a > 0) {
+                    int sub = min(a, bq.top()[1]);
+                    auto s = bq.top();
+                    bq.pop();
+                    s[1] -= sub;
+                    a -= sub;
+                    sum = (sum - sub + mod) % mod;  // Track remaining backlog orders
+                    if (s[1] > 0)
+                        bq.push(s);
+                }
+                if (a > 0) {
+                    sq.push({p, a});
+                    sum = (sum + a) % mod;
+                }
             }
-
         }
-        int res = 0, mod = 1e9 + 7;
-        while (sell.size())
-            res = (res + sell.top()[1]) % mod, sell.pop();
-        while (buy.size())
-            res = (res + buy.top()[1]) % mod, buy.pop();
-        return res;
+
+        return sum;  // Return the total number of remaining backlog orders
     }
 };
