a0cc644bec66993a63955a1b3d597f072d9de676
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-01-31 14:26:15 +0530

    Time: 270 ms (64.71%), Space: 141.9 MB (79.83%) - LeetHub

---
+++ 2920-minimum-seconds-to-equalize-a-circular-array/2920-minimum-seconds-to-equalize-a-circular-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumSeconds(vector<int>& nums) {
+    unordered_map<int, int> pos, gap;
+    for (int i = 0; i < nums.size() * 2; ++i) {
+        int n = nums[i % nums.size()];
+        if (pos.count(n))
+            gap[n] = max(gap[n], (i - pos[n]) / 2);
+        pos[n] = i;
+    }
+    return min_element(begin(gap), end(gap), [](const auto &p1, const auto &p2){
+        return p1.second < p2.second;
+    })->second;
+}
+};
