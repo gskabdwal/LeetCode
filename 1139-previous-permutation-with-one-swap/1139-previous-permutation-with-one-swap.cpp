9fa2610fc502c03453ccc227600906c0b5a157b7
Gaurav Singh Kabdwal <102873484+gskabdwal@users.noreply.github.com>
2025-02-01 13:38:51 +0530

    Time: 0 ms (100%), Space: 29 MB (61.93%) - LeetHub

--- 1139-previous-permutation-with-one-swap/1139-previous-permutation-with-one-swap.cpp
+++ 1139-previous-permutation-with-one-swap/1139-previous-permutation-with-one-swap.cpp
@@ -14,13 +14,13 @@ public:
         if(ind ==-1) return arr;
         int sm = arr[ind],  k = -1;
         for(int i=n-1; i>ind; i--){
-            if((sm==arr[ind]&&sm>arr[i])||(sm != arr[ind]&&sm==arr[i])){
-                sm = arr[i];
-                k = i;
+            if(arr[i]<arr[ind]&& arr[i] != arr[i-1]){
+                swap(arr[ind],arr[i]);
+                break;
             }
         }
 
-        swap(arr[ind],arr[k]);
+        
         return arr;
     }
 };
